1ff19daf9711252702f0c6bd773166b5deaa7df7
Beni Valotker <BeniV@traffilog.com>
2020-02-20 09:04:34 +0200

    version 1.0

---
+++ main_function.py
@@ -0,0 +1,19 @@
+#/path/path/
+#title           :Title 
+#description     :Description.
+#update date     :01/01/2020 12:10
+#version         :1.0
+#changes         :new version changes description.
+#python_version  :3.6  
+#==============================================================================
+
+import json
+import config
+import pyodbc_manager
+
+
+def main_function():
+    try:
+        pass
+    except Exception as ex:
+        pass
---
+++ pyodbc_manager.py
@@ -0,0 +1,48 @@
+import pyodbc
+from config import pyodbc_params	
+
+
+'''
+fast execute many statment  
+    - insert_statement (string):    "INSERT INTO table_name (...) VALUES(?,?,?);"
+    - values (list of tuples):      [(row1), (row2), (row3) .... ] 
+'''
+def sql_insert(insert_statement, values):
+    try:
+        with pyodbc.connect('DRIVER={driver};SERVER={server};DATABASE={data_base};UID={uid};PWD={pwd};Trusted_Connection={trusted_conn};timeout=0;'
+        .format(**pyodbc_params)) as conn:   
+
+            cursor = conn.cursor()
+            cursor.fast_executemany = True
+            cursor.executemany(insert_statement, values)
+            conn.commit()
+
+    except pyodbc.OperationalError as ex:   # errors that are related to the database's operation and not necessarily under the control of the programmer
+        raise ex
+    except pyodbc.InterfaceError as ex:     #  errors that are related to the database interface rather than the database itself
+        raise ex
+    except Exception as ex:                 # other exception
+        raise ex
+
+
+'''
+execute stored procedure 
+    - sp (string):      "exec sp_name  @param_name=?;"
+    - values (tuple):   (ex, ) 
+'''
+def run_sp(sp, values=None):
+    try:
+        with pyodbc.connect('DRIVER={driver};SERVER={server};DATABASE={data_base};UID={uid};PWD={pwd};Trusted_Connection={trusted_conn};timeout=0;'
+        .format(**pyodbc_params)) as conn: 
+
+            cursor = conn.cursor()
+            cursor.fast_executemany = True
+            cursor.execute(sp, values)
+            conn.commit()
+
+    except pyodbc.OperationalError as ex:   # errors that are related to the database's operation and not necessarily under the control of the programmer
+        raise ex
+    except pyodbc.InterfaceError as ex:     #  errors that are related to the database interface rather than the database itself
+        raise ex
+    except Exception as ex:                 # other exception
+        raise ex
